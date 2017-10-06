27e05f593b1c328c211427ea7ca82cab2aa2c5e1
Erik Olsen <eo.olsen@gmail.com>
2017-10-06 11:03:46 +0200

    Add files via upload

---
+++ Application/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogUsers
+{
+    using System.Threading;
+
+    using LogTest;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILog  logger = new AsyncLog();
+
+            for (int i = 0; i < 15; i++)
+            {
+                logger.Write("Number with Flush: " + i.ToString());
+                Thread.Sleep(50);
+            }
+
+            logger.StopWithFlush();
+
+            ILog logger2 = new AsyncLog();
+
+            for (int i = 50; i > 0; i--)
+            {
+                logger2.Write("Number with No flush: " + i.ToString());
+                Thread.Sleep(20);
+            }
+
+            logger2.StopWithoutFlush();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LogTest/ILog.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+namespace LogTest
+{
+    public interface ILog
+    {
+        /// <summary>
+        /// Stop the logging. If any outstadning logs theses will not be written to Log
+        /// </summary>
+        void StopWithoutFlush();
+
+        /// <summary>
+        /// Stop the logging. The call will not return until all all logs have been written to Log.
+        /// </summary>
+        void StopWithFlush();
+
+        /// <summary>
+        /// Write a message to the Log.
+        /// </summary>
+        /// <param name="text">The text to written to the log</param>
+        void Write(string text);
+
+
+    }
+}
